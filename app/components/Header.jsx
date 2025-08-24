0585678590c63f9f41fbe22dc07aca893747a3f4
aleidhussain <aleidhussain202@gmail.com>
2025-08-24 19:01:35 +0300

    modified the next.config.mjs and Header.jsx components

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
             initial={{y: 30, opacity: 0}} 
             whileInView={{y: 0, opacity: 1}}
             transition={{duration: 0.6, delay: 1.2}}
-            href="/CV.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>Download my cv <Image src={assets.download_icon} alt="Right Arrow" className='w-4' /></motion.a>
+            href="./CV.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>Download my cv <Image src={assets.download_icon} alt="Right Arrow" className='w-4' /></motion.a>
         </div>
     </div>
   )
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-output: 'export',
+    output: 'export',
     distDir: 'out',
+    // Only use basePath if deploying to username.github.io/repository-name
+    // Remove these lines if deploying to a custom domain or username.github.io
+    // basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+    // assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
     images: {
         unoptimized: true,
     },
-    
+    trailingSlash: true,
 };
 
 export default nextConfig;
