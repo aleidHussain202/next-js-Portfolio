307fc6dcd29f697564169866aa7b9b3aead016ec
aleidhussain <aleidhussain202@gmail.com>
2025-08-24 19:03:42 +0300

    real next file

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+output: 'export',
     distDir: 'out',
-    // Only use basePath if deploying to username.github.io/repository-name
-    // Remove these lines if deploying to a custom domain or username.github.io
-    // basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
-    // assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/next-js-Portfolio' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/next-js-Portfolio' : '',
     images: {
         unoptimized: true,
     },
